4237274f4eceea7eccf937f04214a7549ea820db
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-07-11 14:12:04 +0200

    Sync Claps with Algolia (#8)

--- models/clap.js
+++ models/clap.js
@@ -1,18 +1,63 @@
 let mongoose = require('mongoose');
+var algolia = require('algoliasearch')(process.env.ALGOLIA_APPLICATION_ID, process.env.ALGOLIA_WRITE_KEY);
+var index = algolia.initIndex('world');
 
 var ClapSchema = mongoose.Schema({
-  organisation: {type: mongoose.Schema.Types.ObjectId, ref: 'Organisation', required: true},
-  giver: {type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true},
-  recipient: {type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true, index: true},
-  hashtag: {type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true, index: true},
-  given: {type: Number, default: 0},
+  organisation: { type: mongoose.Schema.Types.ObjectId, ref: 'Organisation', required: true },
+  giver: { type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true, index: true },
+  hashtag: { type: mongoose.Schema.Types.ObjectId, ref: 'Record', required: true, index: true },
+  given: { type: Number, default: 0 },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
-  owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 // One of the properties chosen here should has index: true
-ClapSchema.index({'recipient': 1, 'giver': 1, 'hashtag': 1}, {unique: false});
+ClapSchema.index({ 'recipient': 1, 'giver': 1, 'hashtag': 1 }, { unique: false });
+
+ClapSchema.methods.algoliaSync = function() {
+  Clap.aggregate(
+    [
+      {
+        $match: {
+          hashtag: this.hashtag,
+          recipient: this.recipient,
+          organisation: this.organisation
+        }
+      },
+      {
+        $group: {
+          _id: "$hashtag",
+          claps: { $sum: "$given" }
+        }
+      }
+    ]).then(claps => {
+      try {
+        var currentClap = claps.find(clap => clap._id.equals(this.hashtag));
+        // pull algolia object
+        index.getObject(this.recipient, ['hashtags'], (err, algoliaObject) => {
+          if (err) throw err;
+          var indexOfWing = algoliaObject.hashtags.findIndex(hashtag => JSON.stringify(hashtag._id) === JSON.stringify(this.hashtag));
+          algoliaObject.hashtags[indexOfWing].claps = currentClap.claps;
+
+          // push update
+          index.partialUpdateObject(algoliaObject, (err, algoliaObjectUpdated) => {
+            if (err) throw err;
+            console.log(`Sync ${algoliaObjectUpdated.objectID} (Clap) with Algolia`);
+          });
+
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }).catch(e => console.log(e));
+}
+
+
+ClapSchema.post('save', (doc) => {
+  doc.algoliaSync();
+})
 
 var LastUpdatedPlugin = require('./plugins/lastUpdated.plugin');
 ClapSchema.plugin(LastUpdatedPlugin);
