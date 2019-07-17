19e25f87803c6c59958286b81be5884950e6048c
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-07-17 12:27:26 +0200

    Update: algolia sync errors handle & record schema (#12)

--- models/record.js
+++ models/record.js
@@ -1,6 +1,38 @@
 var mongoose = require('mongoose');
 
-var recordSchema = mongoose.Schema({});
+var recordSchema = mongoose.Schema({
+  organisation: {type: mongoose.Schema.Types.ObjectId, ref: 'Organisation', default: null, index: true, required: true},
+  tag: {type: String, required: true},
+  type: {type: String, enum: ['person', 'team', 'hashtag']},
+  name: String,
+  name_translated: {
+    en: String,
+    fr: String,
+    'en-UK': String
+  },
+  intro: {type: String},
+  description: {type: String},
+  picture: {
+    url: String,
+    path: String,
+    emoji: String,
+    type: {type: String},
+    uuid: String
+  },
+  cover: {
+    url: String,
+    path: String,
+    type: {type: String},
+    uuid: String
+  },
+  hashtags: [
+    {type: mongoose.Schema.Types.ObjectId, ref: 'Record'}
+  ],
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+  // Hidden is used to control the algolia sync, hidden should be passed to false when user onboard
+  hidden: {type: Boolean, default: false}
+});
 
 var Record = mongoose.model('Record', recordSchema);
 
