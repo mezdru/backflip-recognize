972bb96a916ba4eba88d06bd29edec1c5b857781
mezdru <quentin.drumez@isen.yncrea.fr>
2019-12-05 12:07:23 +0100

    Allow Claps on Wings user hasn't yet

--- api/validation/clap.validation.js
+++ api/validation/clap.validation.js
@@ -12,8 +12,8 @@ router.use(async (req, res, next) => {
 
   if( giver && 
       recipient && 
-      recipient.hashtags.find(hashtag => hashtag.equals(req.body.clap.hashtag) && 
-      req.body.clap.given >= 0) &&
+      // recipient.hashtags.find(hashtag => hashtag.equals(req.body.clap.hashtag)) && 
+      req.body.clap.given > 0 &&
       req.user.orgsAndRecords.find(oar => (oar.record && oar.record.equals(giver._id)) ))
     return next();
 
